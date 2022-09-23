8900487e0e1e73c0fe74ddbbdc1ea49a5c66706c
artssoria <53129227+artssoria@users.noreply.github.com>
2022-09-23 16:06:33 -0300

    app-risa

---
+++ 30-day-of-javascript/introduction.js
@@ -0,0 +1,8 @@
+let nombr = 'arturo'
+let  country = 'jujuy'
+let anio = 100
+const gravedad = 9.81
+const ebullicion = 100
+const PI = 3.14
+console.log(gravedad,ebullicion,PI, anio)
+console.log(nombr, country)
